b8c41a5f2b2871d067e31a77a741b0c4e4b49f62
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-24 14:11:18 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 
+// Created reusable card component
 const Card = (props) => {
 return (
         <View style={styles.containerStyle}>
-            {props.children}
+            {props.children} // Accessed children data using props from parent components 
         </View>
     );
 };
 
+// Card styling
 const styles = {
     containerStyle: {
         flexDirection: 'row',
